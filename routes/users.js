61e43581fc768faf468e3c4fa994f344e46617e5
Joshua Lerner <joshualerner@Joshuas-MacBook-Pro.local>
2017-03-29 16:14:53 -0600

    bookshelf part 3 final

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,23 @@ const express = require('express');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
+const knex = require('../knex');
+const bcrypt = require('bcrypt');
+const humps = require('humps');
 
-// YOUR CODE HERE
+router.post('/users', (req, res) => {
+      knex('users')
+      .returning(['id', 'email', 'first_name', 'last_name'])
+      .insert({
+        'email': req.body.email,
+        'first_name': req.body.firstName,
+        'last_name': req.body.lastName,
+        'hashed_password': bcrypt.hashSync(req.body.password, 8)
+     })
+      .then((data) => {
+         res.send(humps.camelizeKeys(data[0]))
+
+      })
+})
 
 module.exports = router;
