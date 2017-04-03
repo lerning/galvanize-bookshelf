6f9ea81f328ab13f420c7936ac41e2c3a47463b2
Joshua Lerner <joshualerner@Joshuas-MacBook-Pro.local>
2017-04-02 19:47:19 -0600

    part 4: working on post

--- routes/token.js
+++ routes/token.js
@@ -1,10 +1,59 @@
 'use strict';
 
 const express = require('express');
+const app = express();
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
+const knex = require('knex');
+const bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
 
-// YOUR CODE HERE
+var cookieSession = require('cookie-session');
+
+// app.use(cookieSession({
+//   name: 'session',
+//   keys: ['Gotham','Batman'],
+// }))
+
+
+router.get('/', (req, res) => {
+   if (req.cookies.token == undefined){
+      res.status(200).send(false)
+   } else {
+      res.status(200).send(true)
+   }
+})
+
+router.post('/', (req, res) => {
+   let email = req.body.email
+   let password = req.body.password
+   // if (!email){
+   //    console.log('bad email caught');
+   //    res.sendStatus(400, 'Bad email or password');
+   // }
+
+   console.log('email', email);
+   console.log('password', password);
+   console.log('req body', req.body);
+
+   knex('users')
+      .where ('email', email)
+      .then((user) => {
+         if(user.length === 0){
+           res.type('text/plain');
+           res.status(400);
+           res.send("Bad email or password");
+         }
+      })
+
+      var token = jwt.sign ({email: email, password: password}, 'sh');
+      console.log('token', token);
+
+   res.cookie('token', token);
+   res.send()
+
+
+})
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -10,16 +10,15 @@ const humps = require('humps');
 
 router.post('/users', (req, res) => {
       knex('users')
-      .returning(['id', 'email', 'first_name', 'last_name'])
-      .insert({
-        'email': req.body.email,
-        'first_name': req.body.firstName,
-        'last_name': req.body.lastName,
-        'hashed_password': bcrypt.hashSync(req.body.password, 8)
-     })
-      .then((data) => {
-         res.send(humps.camelizeKeys(data[0]))
-
+         .returning(['id', 'email', 'first_name', 'last_name'])
+         .insert({
+           'email': req.body.email,
+           'first_name': req.body.firstName,
+           'last_name': req.body.lastName,
+           'hashed_password': bcrypt.hashSync(req.body.password, 8)
+        })
+         .then((data) => {
+            res.send(humps.camelizeKeys(data[0]))
       })
 })
 
