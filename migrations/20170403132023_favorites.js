064154712609cf756baceb9de8d02ce6dac81a81
Joshua Lerner <joshualerner@Joshuas-MacBook-Pro.local>
2017-04-03 13:38:40 -0600

    part 5 migrations/seeds complete

---
+++ migrations/20170403132023_favorites.js
@@ -0,0 +1,13 @@
+exports.up = function(knex) {
+   return knex.schema.createTable('favorites', (table) => {
+      table.increments();
+      table.integer('book_id').references('id').inTable('books').notNullable().onDelete('CASCADE').index();
+      table.integer('user_id').references('id').inTable('users').notNullable().onDelete('CASCADE').index();
+      table.timestamps(true, true);
+   });
+
+};
+
+exports.down = function(knex) {
+   return knex.schema.dropTable('favorites');
+};
